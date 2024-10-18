372c8bb5d8ba805ad64faf9a4cf984163630cc43
Sean O'Connor <so@seano.dev>
2024-10-18 16:40:12 -0600

    Create SecurityFilterChain bean

---
+++ src/main/java/com/aniable/anibl/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2024 Sean O'Connor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.aniable.anibl.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.cors(CorsConfigurer::disable)
+					.csrf(CsrfConfigurer::disable)
+					.formLogin(FormLoginConfigurer::disable)
+					.httpBasic(HttpBasicConfigurer::disable)
+					.authorizeHttpRequests(requests -> requests.anyRequest().permitAll())
+					.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		return httpSecurity.build();
+	}
+}
