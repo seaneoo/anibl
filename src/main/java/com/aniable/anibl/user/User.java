46a4bc7b3383efc8854a3a3df5f369631e3b84d1
Sean O'Connor <so@seano.dev>
2024-10-18 23:40:58 -0400

    Implement OAuth2User in User entity

--- src/main/java/com/aniable/anibl/user/User.java
+++ src/main/java/com/aniable/anibl/user/User.java
@@ -26,8 +26,13 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.time.ZonedDateTime;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Table(name = "users", uniqueConstraints = {@UniqueConstraint(columnNames = {"provider", "provider_id"})})
@@ -37,7 +42,7 @@ import java.util.UUID;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class User implements OAuth2User {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.UUID)
@@ -50,6 +55,9 @@ public class User {
 	@Column(nullable = false)
 	private String providerId;
 
+	@Column(nullable = false)
+	private String name;
+
 	@Column(nullable = false, updatable = false)
 	@CreatedDate
 	private ZonedDateTime created;
@@ -57,4 +65,14 @@ public class User {
 	@Column(nullable = false)
 	@LastModifiedDate
 	private ZonedDateTime lastModified;
+
+	@Override
+	public Map<String, Object> getAttributes() {
+		return Map.of();
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of();
+	}
 }
