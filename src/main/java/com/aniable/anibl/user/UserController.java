e116af5cd37539056df8619ffe55d0e7a8ac05e5
Sean O'Connor <so@seano.dev>
2024-10-19 15:40:25 -0600

    Add User controller & authenticated user endpoint

---
+++ src/main/java/com/aniable/anibl/user/UserController.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2024 Sean O'Connor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.aniable.anibl.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+	@GetMapping
+	public ResponseEntity<User> getAuthenticatedUser(@AuthenticationPrincipal User user) {
+		return ResponseEntity.ok(user);
+	}
+}
