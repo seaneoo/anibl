a2a15cec43a70620c9d483e9e29b46175c559a5c
Sean O'Connor <so@seano.dev>
2024-10-18 21:25:32 -0400

    Enable JPA auditing & create DateTimeProvider bean

---
+++ src/main/java/com/aniable/anibl/config/JpaConfig.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2024 Sean O'Connor
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.aniable.anibl.config;
+
+import com.aniable.anibl.util.DateTimeUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.auditing.DateTimeProvider;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.Optional;
+
+@Configuration
+@EnableJpaAuditing(dateTimeProviderRef = "utcDateTimeProvider")
+public class JpaConfig {
+
+	@Bean
+	DateTimeProvider utcDateTimeProvider() {
+		return () -> Optional.of(DateTimeUtils.now());
+	}
+}
